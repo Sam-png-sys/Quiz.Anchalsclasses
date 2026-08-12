57efe9f2c755224aebcc053a63e9350d2d007ec5
Gaurav-Kanse <gauravkanse27@gmail.com>
2026-08-12 18:41:10 +0530

    Add react-native-gesture-handler import to App.js to prevent navigation crash

--- quiz-app/App.js
+++ quiz-app/App.js
@@ -1,3 +1,4 @@
+import "react-native-gesture-handler";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { AuthProvider } from "./src/context/AuthContext";
